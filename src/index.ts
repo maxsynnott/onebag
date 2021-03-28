9d12bc2c5bc4b349330c98ade092704e5113c5f0
maxsynnott <maxryansynnott@gmail.com>
2021-03-28 12:49:48 +0200

    Nodemon config

--- src/index.ts
+++ src/index.ts
@@ -37,26 +37,8 @@ createConnection()
 		// ...
 
 		// start express server
-		app.listen(3000)
-
-		// insert new users for test
-		await connection.manager.save(
-			connection.manager.create(User, {
-				firstName: 'Timber',
-				lastName: 'Saw',
-				age: 27,
-			}),
-		)
-		await connection.manager.save(
-			connection.manager.create(User, {
-				firstName: 'Phantom',
-				lastName: 'Assassin',
-				age: 24,
-			}),
-		)
-
-		console.log(
-			'Express server has started on port 3000. Open http://localhost:3000/users to see results',
-		)
+		app.listen(3000, () => {
+			console.log('Listening on port: 3000')
+		})
 	})
 	.catch((error) => console.log(error))
