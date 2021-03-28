b6be999fdd48b1ce5e159245d05a929d0aceb891
maxsynnott <maxryansynnott@gmail.com>
2021-03-28 17:20:26 +0200

    Added users/current route

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { UserService } from '../services/UserService'
+import { RequestWithUser } from '../types'
 
 export class UserController {
 	async create(req: Request, res: Response) {
@@ -8,5 +9,12 @@ export class UserController {
 		const user = await userService.create(req.body)
 
 		res.json(user)
+	}
+
+	async current(req: RequestWithUser, res: Response) {
+		if (!req.user) return res.json(null)
+
+		const { passwordHash, ...filteredUser } = req.user
+		res.json(filteredUser)
 	}
 }
--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,9 +1,12 @@
 import { UserController } from '../controllers/UserController'
 import { Router } from 'express'
+import passport from 'passport'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
 const controller = new UserController()
 
 const router = Router()
 router.post('/', controller.create)
+router.get('/current', controller.current)
 
 export default router
