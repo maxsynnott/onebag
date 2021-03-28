d71a95af77672de79a825e105b3fbc0e279e520e
maxsynnott <maxryansynnott@gmail.com>
2021-03-28 15:40:27 +0200

    Removing generated files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes'
 import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core'
